87ddc695ec343f947c97f9598d831809b8a46e93
Kizito Nzeka <kizzyche@ymail.com>
2020-04-10 13:31:41 +0100

    Add validation to app.js

--- app.js
+++ app.js
@@ -2,15 +2,38 @@
  const express = require("express");
  const app = express();
  const bodyParser = require("body-parser");
+ const joi = require("joi");
+ const https = require("https");
  const request = require("request");
  const port = process.env.PORT || 3000 ;
 
  app.use(bodyParser.urlencoded({extended: true}))
 
- app.use(express.static('/public'))
+ app.use(express.static('/public'));
 
  app.get("/", function(req, res){
      res.sendFile(__dirname + "/signup.html")
+ })
+
+ app.post("/", function(req, res){
+
+    const schema = {
+        fname: joi.string(),
+        lname: joi.string(),
+        email: joi.string().email({minDomainAtoms: 2})
+    }
+
+    const result = joi.validate({fname: req.body.fname, lname: req.body.lname, email: req.body.email}, schema);
+
+    if(result.error){
+        res.sendFile(__dirname + "/fail.html");
+    }else{
+       
+
+        
+    }
+
+     
  })
 
  app.listen(port, function(){
